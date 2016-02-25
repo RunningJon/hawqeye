e20eaf03e61e4c3b590f269731de6e96a3c6cebe
jgronline@gmail.com <jgronline@gmail.com>
2016-02-25 10:07:27 -0600

    0.0.7

---
+++ hawqeye.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+set -e
+
+PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+GEN_DATA_SCALE=$1
+if [ "$GEN_DATA_SCALE" == "" ]; then
+	echo "You must provide the scale as a parameter in terms of Gigabytes."
+	echo "Example: ./tpcds.sh 100"
+	echo "This will create 100 GB of data for this test."
+	exit 1
+fi
+QUIET=$2
+
+MYCMD="hawqeye.sh"
+MYVAR="tpcds_variables.sh"
+REPO="hawqeye"
+REPO_URL="https://github.com/pivotalguru/hawqeye"
+GIT_SSL_NO_VERIFY="true"
+ADMIN_USER=$(echo $USER)
+
+##################################################################################################################################################
+# Functions
+##################################################################################################################################################
+
+yum_checks()
+{
+	echo "############################################################################"
+	echo "Make sure git and gcc is installed."
+	echo "############################################################################"
+	echo ""
+	# Install git and gcc if not found
+	local CURL_INSTALLED=$(gcc --help 2> /dev/null | wc -l)
+	local GIT_INSTALLED=$(git --help 2> /dev/null | wc -l)
+
+	if [ "$CURL_INSTALLED" -eq "0" ]; then
+		echo "gcc not installed.  Please install gcc and try again."
+		exit 1
+	fi
+	if [ "$GIT_INSTALLED" -eq "0" ]; then
+		echo "git not installed.  Please install git and try again."
+		exit 1
+	fi
+	echo ""
+}
+
+repo_init()
+{
+	### Install repo ###
+	echo "############################################################################"
+	echo "Install the github repository."
+	echo "############################################################################"
+	echo ""
+
+	internet_down="0"
+	for j in $(curl google.com 2>&1 | grep "Could not resolve host"); do
+		internet_down="1"
+	done
+
+	if [ ! -d $PWD/$REPO ]; then
+		if [ "$internet_down" -eq "1" ]; then
+			echo "Unable to continue because repo hasn't been downloaded and Internet is not available."
+			exit 1
+		else
+			echo ""
+			echo "Creating $REPO directory"
+			echo "-------------------------------------------------------------------------"
+			mkdir $PWD/$REPO
+			git clone --depth=1 $REPO_URL
+		fi
+	else
+		if [ "$internet_down" -eq "0" ]; then
+			git config --global user.email "$ADMIN_USER@$HOSTNAME"
+			git config --global user.name "$ADMIN_USER"
+			cd $PWD/$REPO
+			git fetch --all
+			git reset --hard origin/master
+			cd ..
+		fi
+	fi
+}
+
+script_check()
+{
+	### Make sure the repo doesn't have a newer version of this script. ###
+	echo "############################################################################"
+	echo "Make sure this script is up to date."
+	echo "############################################################################"
+	echo ""
+	# Must be executed after the repo has been pulled
+	local d=`diff $PWD/$MYCMD $PWD/$REPO/$MYCMD | wc -l`
+
+	if [ "$d" -eq "0" ]; then
+		echo "$MYCMD script is up to date so continuing."
+		echo ""
+	else
+		echo "$MYCMD script is NOT up to date."
+		echo ""
+		cp $PWD/$REPO/$MYCMD $PWD/$MYCMD
+		echo "After this script completes, restart the $MYCMD with this command:"
+		echo "./$MYCMD"
+		exit 1
+	fi
+
+}
+
+echo_variables()
+{
+	echo "############################################################################"
+	echo "REPO: $REPO"
+	echo "REPO_URL: $REPO_URL"
+	echo "ADMIN_USER: $ADMIN_USER"
+	echo "############################################################################"
+	echo ""
+}
+
+##################################################################################################################################################
+# Body
+##################################################################################################################################################
+
+yum_checks
+repo_init
+script_check
+echo_variables
+
+echo "Done!"
