1e239ecab2c4bc78387ebdfed995d31860c286e3
jgronline@gmail.com <jgronline@gmail.com>
2016-03-01 10:16:43 -0600

    0.0.26

--- impala-tpcds/00_compile_tpcds/rollout.sh
+++ impala-tpcds/00_compile_tpcds/rollout.sh
@@ -18,14 +18,14 @@ make_tpc()
 copy_tpc()
 {
 	cp $PWD/tools/dsqgen ../*gen_data/
-	cp $PWD/tools/dsdgen ../*gen_data/
+	cp $PWD/tools/dsqgen ../testing/
 	cp $PWD/tools/tpcds.idx ../*gen_data/
 	cp $PWD/tools/tpcds.idx ../testing/
 
 	#copy the compiled dsdgen program to the datanodes
 	for i in $(cat $PWD/../dn.txt); do
 		echo "copy tpcds binaries to $i:$ADMIN_HOME"
-		scp tools/dsdgen tools/dsqgen tools/tpcds.idx $i:$ADMIN_HOME/
+		scp tools/dsdgen tools/tpcds.idx $i:$ADMIN_HOME/
 	done
 }
 
