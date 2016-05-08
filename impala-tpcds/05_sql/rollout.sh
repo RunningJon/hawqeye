e6bb224ade3b0b6ef212fee6f6a5f7cf84045cbc
Jon Roberts <jgronline@gmail.com>
2016-05-08 15:17:35 -0400

    0.0.68

--- impala-tpcds/05_sql/rollout.sh
+++ impala-tpcds/05_sql/rollout.sh
@@ -32,6 +32,7 @@ for i in $(ls $PWD/*.$SQL_VERSION.*.sql); do
 		error_communicate_impalad_count=$(grep "Error communicating with impalad" $query_log_file | wc -l)
 		error_connection_reset_count=$(grep "Socket error 104: Connection reset by peer" $query_log_file | wc -l)
 		error_connection_refused_count=$(grep "Connection refused" $query_log_file | wc -l)
+		warning_unreachable_impalad=$(grep "Cancelled due to unreachable impalad" $query_log_file | wc -l)
 
 		# out of memory error happens on queries under heavy load.  Continue with these queries.
 		oom_count=$(grep "Memory limit exceeded" $query_log_file | wc -l)
@@ -43,7 +44,7 @@ for i in $(ls $PWD/*.$SQL_VERSION.*.sql); do
 			run_query="0"
 			log $tuples
 		else
-			if [[ "$error_state_store_count" -gt "0" || "$error_connect_timeout_count" -gt "0" || "$error_communicate_impalad_count" -gt "0" || "$error_connection_reset_count" -gt "0" || "$error_connection_refused_count" -gt "0" ]]; then
+			if [[ "$error_state_store_count" -gt "0" || "$error_connect_timeout_count" -gt "0" || "$error_communicate_impalad_count" -gt "0" || "$error_connection_reset_count" -gt "0" || "$error_connection_refused_count" -gt "0" || "$warning_unreachable_impalad" -gt "0" ]]; then
 
 				#Wait 5 seconds and try again
 				#Print the error message and continue
@@ -65,6 +66,10 @@ for i in $(ls $PWD/*.$SQL_VERSION.*.sql); do
 
 				if [ "$error_connection_refused_count" -gt "0" ]; then
 					grep "Connection refused" $query_log_file
+				fi
+
+				if [ "$warning_unreachable_impalad" -gt "0" ]; then
+					grep "Cancelled due to unreachable impalad" $query_log_file
 				fi
 
 				#capture the execution time and if too long, then don't retry
