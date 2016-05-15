28c80291e094afdc25bfc778da87901a2909b690
Jon Roberts <jgronline@gmail.com>
2016-05-15 03:50:34 -0400

    0.0.76

--- impala-tpcds/05_sql/rollout.sh
+++ impala-tpcds/05_sql/rollout.sh
@@ -20,18 +20,18 @@ for i in $(ls $PWD/*.$SQL_VERSION.*.sql); do
 	myfile=$(basename $i)
 
 	run_query="1"
-	oom_count="0"
+	oom="0"
 	while [ "$run_query" -eq "1" ]; do
 		query_log_file="$PWD/../log/one_session_""$myfile"".log"
 		echo "impala-shell -i $IMP_HOST -d $TPCDS_DBNAME -f $i --quiet"
 		impala-shell -i $IMP_HOST -d $TPCDS_DBNAME -f $i --quiet > $query_log_file 2>&1 || true
 
 		# these are known errors that Impala will have under heavy load.  When this happens, retry to run the query
-		error_state_store_count=$(grep "Waiting for catalog update from the StateStore" $query_log_file | wc -l)
-		error_connect_timeout_count=$(grep "Error connecting: TTransportException" $query_log_file | wc -l)
-		error_communicate_impalad_count=$(grep "Error communicating with impalad" $query_log_file | wc -l)
-		error_connection_reset_count=$(grep "Socket error 104: Connection reset by peer" $query_log_file | wc -l)
-		error_connection_refused_count=$(grep "Connection refused" $query_log_file | wc -l)
+		error_state_store=$(grep "Waiting for catalog update from the StateStore" $query_log_file | wc -l)
+		error_connect_timeout=$(grep "Error connecting: TTransportException" $query_log_file | wc -l)
+		error_communicate_impalad=$(grep "Error communicating with impalad" $query_log_file | wc -l)
+		error_connection_reset=$(grep "Socket error 104: Connection reset by peer" $query_log_file | wc -l)
+		error_connection_refused=$(grep "Connection refused" $query_log_file | wc -l)
 		error_unreachable_impalad=$(grep "Cancelled due to unreachable impalad" $query_log_file | wc -l)
 		error_econnreset=$(grep "ECONNRESET" $query_log_file | wc -l)
 		error_sender_timeout=$(grep "Sender timed out waiting for receiver fragment instance" $query_log_file | wc -l)
@@ -49,15 +49,12 @@ for i in $(ls $PWD/*.$SQL_VERSION.*.sql); do
 		unsupported_return_type=$(grep "Incompatible return types 'INT' and 'ARRAY<BIGINT>'" $query_log_file | wc -l)
 		unsupported_except=$(grep "except" $query_log_file | wc -l)
 
-		# check for any error because there might be one that is not expected
-		error_count=$(grep -i error $query_log_file | wc -l)
-
 		# out of memory error happens on queries under heavy load.  Continue with these queries.
-		oom_count=$(grep "Memory limit exceeded" $query_log_file | wc -l)
+		oom=$(grep "Memory limit exceeded" $query_log_file | wc -l)
 
 		#OOM and unsupported SQL
-		if [[ "$oom_count" -gt "0" || "$unsupported_alias" -gt "0" || "$unsupported_subquery_one_row" -gt "0" || "$unsupported_intersect" -gt "0" || "$unsupported_subquery_select_list" -gt "0" || "$unsupported_subquery_in_or" -gt "0" || "$unsupported_rollup" -gt "0" || "$unsupported_subquery_having" -gt "0" || "$unsupported_grouping" -gt "0" || "$unsupported_correlated_predicates" -gt "0" || "$unsupported_return_type" -gt "0" || "$unsupported_except" -gt "0" ]]; then
-			if [ "$oom_count" -gt "0" ]; then
+		if [[ "$oom" -gt "0" || "$unsupported_alias" -gt "0" || "$unsupported_subquery_one_row" -gt "0" || "$unsupported_intersect" -gt "0" || "$unsupported_subquery_select_list" -gt "0" || "$unsupported_subquery_in_or" -gt "0" || "$unsupported_rollup" -gt "0" || "$unsupported_subquery_having" -gt "0" || "$unsupported_grouping" -gt "0" || "$unsupported_correlated_predicates" -gt "0" || "$unsupported_return_type" -gt "0" || "$unsupported_except" -gt "0" ]]; then
+			if [ "$oom" -gt "0" ]; then
 				grep "Memory limit exceeded" $query_log_file
 			fi
 			if [ "$unsupported_alias" -gt "0" ]; then
@@ -99,22 +96,22 @@ for i in $(ls $PWD/*.$SQL_VERSION.*.sql); do
 			log $tuples
 		else
 			#Errors that warrant a retry
-			if [[ "$error_state_store_count" -gt "0" || "$error_connect_timeout_count" -gt "0" || "$error_communicate_impalad_count" -gt "0" || "$error_connection_reset_count" -gt "0" || "$error_connection_refused_count" -gt "0" || "$error_unreachable_impalad" -gt "0" || "$error_econnreset" -gt "0" || "$error_count" -gt "0" || "$error_sender_timeout" -gt "0" ]]; then
+			if [[ "$error_state_store" -gt "0" || "$error_connect_timeout" -gt "0" || "$error_communicate_impalad" -gt "0" || "$error_connection_reset" -gt "0" || "$error_connection_refused" -gt "0" || "$error_unreachable_impalad" -gt "0" || "$error_econnreset" -gt "0" || "$error_sender_timeout" -gt "0" ]]; then
 				# Wait 5 seconds and try again
 				# Print the error message and continue
-				if [ "$error_state_store_count" -gt "0" ]; then
+				if [ "$error_state_store" -gt "0" ]; then
 					grep "Waiting for catalog update from the StateStore" $query_log_file
 				fi
-				if [ "$error_connect_timeout_count" -gt "0" ]; then
+				if [ "$error_connect_timeout" -gt "0" ]; then
 					grep "Error connecting: TTransportException" $query_log_file
 				fi
-				if [ "$error_communicate_impalad_count" -gt "0" ]; then
+				if [ "$error_communicate_impalad" -gt "0" ]; then
 					grep "Error communicating with impalad" $query_log_file
 				fi
-				if [ "$error_connection_reset_count" -gt "0" ]; then
+				if [ "$error_connection_reset" -gt "0" ]; then
 					grep "Socket error 104: Connection reset by peer" $query_log_file
 				fi
-				if [ "$error_connection_refused_count" -gt "0" ]; then
+				if [ "$error_connection_refused" -gt "0" ]; then
 					grep "Connection refused" $query_log_file
 				fi
 				if [ "$error_unreachable_impalad" -gt "0" ]; then
@@ -125,11 +122,6 @@ for i in $(ls $PWD/*.$SQL_VERSION.*.sql); do
 				fi
 				if [ "$error_sender_timeout" -gt "0" ]; then
 					grep "Sender timed out waiting for receiver fragment instance" $query_log_file
-				fi
-				# check for unexpected errors
-				if [[ "$error_state_store_count" -eq "0" && "$error_connect_timeout_count" -eq "0" && "$error_communicate_impalad_count" -eq "0" && "$error_connection_reset_count" -eq "0" && "$error_connection_refused_count" -eq "0" && "$error_unreachable_impalad" -eq "0" && "$error_econnreset" -eq "0" && "$error_sender_timeout" -eq "0" && "$oom_count" -eq "0" && "$unsupported_alias" -eq "0" && "$unsupported_subquery_one_row" -eq "0" && "$unsupported_intersect" -eq "0" && "$unsupported_subquery_select_list" -eq "0" && "$unsupported_subquery_in_or" -eq "0" && "$unsupported_rollup" -eq "0" && "$unsupported_subquery_having" -eq "0" && "$unsupported_grouping" -eq "0" && "$unsupported_correlated_predicates" -eq "0" && "$unsupported_return_type" -eq "0" && "$unsupported_except" -eq "0" && "$error_count" -gt "0" ]]; then
-					echo "Unexpected error!"
-					grep -i error $query_log_file
 				fi
 				# capture the execution time and if too long, then don't retry
 				if [ "$OSVERSION" == "Linux" ]; then
--- impala-tpcds/07_multi_user/test.sh
+++ impala-tpcds/07_multi_user/test.sh
@@ -67,18 +67,18 @@ for i in $(ls $sql_dir/*.sql); do
 	table_name=$(basename $i | awk -F '.' '{print $3}')
 
 	run_query="1"
-	oom_count="0"
+	oom="0"
 	while [ "$run_query" -eq "1" ]; do
 		query_log_file="$PWD/../log/test_""$session_id""_""$table_name"".log"
 		echo "impala-shell -i $IMP_HOST -d $TPCDS_DBNAME -f $i --quiet"
 		impala-shell -i $IMP_HOST -d $TPCDS_DBNAME -f $i --quiet > $query_log_file 2>&1 || true
 
 		# these are known errors that Impala will have under heavy load.  When this happens, retry to run the query
-		error_state_store_count=$(grep "Waiting for catalog update from the StateStore" $query_log_file | wc -l)
-		error_connect_timeout_count=$(grep "Error connecting: TTransportException" $query_log_file | wc -l)
-		error_communicate_impalad_count=$(grep "Error communicating with impalad" $query_log_file | wc -l)
-		error_connection_reset_count=$(grep "Socket error 104: Connection reset by peer" $query_log_file | wc -l)
-		error_connection_refused_count=$(grep "Connection refused" $query_log_file | wc -l)
+		error_state_store=$(grep "Waiting for catalog update from the StateStore" $query_log_file | wc -l)
+		error_connect_timeout=$(grep "Error connecting: TTransportException" $query_log_file | wc -l)
+		error_communicate_impalad=$(grep "Error communicating with impalad" $query_log_file | wc -l)
+		error_connection_reset=$(grep "Socket error 104: Connection reset by peer" $query_log_file | wc -l)
+		error_connection_refused=$(grep "Connection refused" $query_log_file | wc -l)
 		error_unreachable_impalad=$(grep "Cancelled due to unreachable impalad" $query_log_file | wc -l)
 		error_econnreset=$(grep "ECONNRESET" $query_log_file | wc -l)
 		error_sender_timeout=$(grep "Sender timed out waiting for receiver fragment instance" $query_log_file | wc -l)
@@ -96,16 +96,13 @@ for i in $(ls $sql_dir/*.sql); do
 		unsupported_return_type=$(grep "Incompatible return types 'INT' and 'ARRAY<BIGINT>'" $query_log_file | wc -l)
 		unsupported_except=$(grep "except" $query_log_file | wc -l)
 
-		# check for any error because there might be one that is not expected
-		error_count=$(grep -i error $query_log_file | wc -l)
-
 		# out of memory error happens on some queries and won't finish no matter what.  
 		# continue when OOM occurs
-		oom_count=$(grep "Memory limit exceeded" $query_log_file | wc -l)
+		oom=$(grep "Memory limit exceeded" $query_log_file | wc -l)
 
 		#OOM and unsupported SQL	
-		if [[ "$oom_count" -gt "0" || "$unsupported_alias" -gt "0" || "$unsupported_subquery_one_row" -gt "0" || "$unsupported_intersect" -gt "0" || "$unsupported_subquery_select_list" -gt "0" || "$unsupported_subquery_in_or" -gt "0" || "$unsupported_rollup" -gt "0" || "$unsupported_subquery_having" -gt "0" || "$unsupported_grouping" -gt "0" || "$unsupported_correlated_predicates" -gt "0" || "$unsupported_return_type" -gt "0" || "$unsupported_except" -gt "0" ]]; then
-			if [ "$oom_count" -gt "0" ]; then
+		if [[ "$oom" -gt "0" || "$unsupported_alias" -gt "0" || "$unsupported_subquery_one_row" -gt "0" || "$unsupported_intersect" -gt "0" || "$unsupported_subquery_select_list" -gt "0" || "$unsupported_subquery_in_or" -gt "0" || "$unsupported_rollup" -gt "0" || "$unsupported_subquery_having" -gt "0" || "$unsupported_grouping" -gt "0" || "$unsupported_correlated_predicates" -gt "0" || "$unsupported_return_type" -gt "0" || "$unsupported_except" -gt "0" ]]; then
+			if [ "$oom" -gt "0" ]; then
 				grep "Memory limit exceeded" $query_log_file
 			fi
 			if [ "$unsupported_alias" -gt "0" ]; then
@@ -147,22 +144,22 @@ for i in $(ls $sql_dir/*.sql); do
 			log $tuples
 		else
 			#Errors that warrant a retry
-			if [[ "$error_state_store_count" -gt "0" || "$error_connect_timeout_count" -gt "0" || "$error_communicate_impalad_count" -gt "0" || "$error_connection_reset_count" -gt "0" || "$error_connection_refused_count" -gt "0" || "$error_unreachable_impalad" -gt "0" || "$error_econnreset" -gt "0" || "$error_count" -gt "0" || "$error_sender_timeout" -gt "0" ]]; then
+			if [[ "$error_state_store" -gt "0" || "$error_connect_timeout" -gt "0" || "$error_communicate_impalad" -gt "0" || "$error_connection_reset" -gt "0" || "$error_connection_refused" -gt "0" || "$error_unreachable_impalad" -gt "0" || "$error_econnreset" -gt "0" || "$error_sender_timeout" -gt "0" ]]; then
 				# Wait 5 seconds and try again
 				# Print the error message and continue
-				if [ "$error_state_store_count" -gt "0" ]; then
+				if [ "$error_state_store" -gt "0" ]; then
 					grep "Waiting for catalog update from the StateStore" $query_log_file 
 				fi
-				if [ "$error_connect_timeout_count" -gt "0" ]; then 
+				if [ "$error_connect_timeout" -gt "0" ]; then 
 					grep "Error connecting: TTransportException" $query_log_file
 				fi
-				if [ "$error_communicate_impalad_count" -gt "0" ]; then 
+				if [ "$error_communicate_impalad" -gt "0" ]; then 
 					grep "Error communicating with impalad" $query_log_file
 				fi
-				if [ "$error_connection_reset_count" -gt "0" ]; then 
+				if [ "$error_connection_reset" -gt "0" ]; then 
 					grep "Socket error 104: Connection reset by peer" $query_log_file
 				fi
-				if [ "$error_connection_refused_count" -gt "0" ]; then 
+				if [ "$error_connection_refused" -gt "0" ]; then 
 					grep "Connection refused" $query_log_file
 				fi
 				if [ "$error_unreachable_impalad" -gt "0" ]; then
@@ -173,11 +170,6 @@ for i in $(ls $sql_dir/*.sql); do
 				fi
 				if [ "$error_sender_timeout" -gt "0" ]; then
 					grep "Sender timed out waiting for receiver fragment instance" $query_log_file
-				fi
-				# check for unexpected errors
-				if [[ "$error_state_store_count" -eq "0" && "$error_connect_timeout_count" -eq "0" && "$error_communicate_impalad_count" -eq "0" && "$error_connection_reset_count" -eq "0" && "$error_connection_refused_count" -eq "0" && "$error_unreachable_impalad" -eq "0" && "$error_econnreset" -eq "0" && "$error_sender_timeout" -eq "0" && "$oom_count" -eq "0" && "$unsupported_alias" -eq "0" && "$unsupported_subquery_one_row" -eq "0" && "$unsupported_intersect" -eq "0" && "$unsupported_subquery_select_list" -eq "0" && "$unsupported_subquery_in_or" -eq "0" && "$unsupported_rollup" -eq "0" && "$unsupported_subquery_having" -eq "0" && "$unsupported_grouping" -eq "0" && "$unsupported_correlated_predicates" -eq "0" && "$unsupported_return_type" -eq "0" && "$unsupported_except" -eq "0" && "$error_count" -gt "0" ]]; then
-					echo "Unexpected error!"
-					grep -i error $query_log_file
 				fi
 				# capture the execution time and if too long, then don't retry
 				if [ "$OSVERSION" == "Linux" ]; then
