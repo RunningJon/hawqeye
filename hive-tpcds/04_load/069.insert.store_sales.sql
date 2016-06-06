bb77b1e9ae3f4a90cd70d135951902470cbbf567
Jon Roberts <jgronline@gmail.com>
2016-06-06 12:23:21 -0400

    0.0.81

--- hive-tpcds/04_load/069.insert.store_sales.sql
+++ hive-tpcds/04_load/069.insert.store_sales.sql
@@ -1,19 +1,3 @@
-set hive.exec.dynamic.partition.mode=nonstrict;
-set hive.exec.max.dynamic.partitions.pernode=100000;
-set hive.exec.max.dynamic.partitions=100000;
-set hive.exec.max.created.files=1000000;
-set hive.exec.parallel=true;
-set hive.exec.reducers.max=1;
-set hive.stats.autogather=true;
-set hive.optimize.sort.dynamic.partition=true;
-
-set mapred.job.reduce.input.buffer.percent=0.0;
-set mapreduce.input.fileinputformat.split.minsizee=240000000;
-set mapreduce.input.fileinputformat.split.minsize.per.node=240000000;
-set mapreduce.input.fileinputformat.split.minsize.per.rack=240000000;
-set hive.optimize.sort.dynamic.partition=true;
-set hive.tez.java.opts=-XX:+PrintGCDetails -verbose:gc -XX:+PrintGCTimeStamps -XX:+UseNUMA -XX:+UseG1GC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/;
-
 INSERT INTO store_sales
 PARTITION(ss_sold_date_sk) 
 SELECT 
